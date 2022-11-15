feaf8c371a8e28a80630fd18fa769c9723b1df26
David Schultz <david.schultz@icecube.wisc.edu>
2022-11-15 14:16:14 -0600

    breadcrumbs defaults to the page title if empty

--- includes/breadcrumbs.php
+++ includes/breadcrumbs.php
@@ -1,7 +1,12 @@
 <?php
 
 function my_bcn_template_tag( $replacements, $type, $id ) {
-    $replacements['%breadcrumb%'] = the_field( 'breadcrumb' );
+    $breadcrumb = the_field( 'breadcrumb' );
+    if ( $breadcrumb == '' ) {
+        $breadcrumb = isset( $post->post_title ) ? $post->post_title : '';
+    }
+
+    $replacements['%breadcrumb%'] = $breadcrumb;
     return $replacements;
 }
 add_filter( 'bcn_template_tags', 'my_bcn_template_tag', 3, 1 );
