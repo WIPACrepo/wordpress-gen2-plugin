b0aba8ac6e12bb936f17e57f73e82dd936f16551
David Schultz <david.schultz@icecube.wisc.edu>
2022-11-15 14:00:18 -0600

    fix php lint

--- includes/breadcrumbs.php
+++ includes/breadcrumbs.php
@@ -1,8 +1,7 @@
 <?php
 
-function my_bcn_template_tag($replacements, $type, $id)
-{
-    $replacements['%breadcrumb%'] = the_field('breadcrumb');
+function my_bcn_template_tag( $replacements, $type, $id ) {
+    $replacements['%breadcrumb%'] = the_field( 'breadcrumb' );
     return $replacements;
 }
-add_filter('bcn_template_tags', 'my_bcn_template_tag', 3, 1);
+add_filter( 'bcn_template_tags', 'my_bcn_template_tag', 3, 1 );
