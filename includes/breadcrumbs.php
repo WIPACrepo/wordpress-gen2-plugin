c1d8829ad6a1dc0747c68142004c1ed0aed67791
David Schultz <davids24@gmail.com>
2022-11-15 14:38:13 -0600

    add breadcrumb field (#3)

---
+++ includes/breadcrumbs.php
@@ -0,0 +1,12 @@
+<?php
+
+function my_bcn_template_tag( $replacements, $type, $id ) {
+    $breadcrumb = the_field( 'breadcrumb' );
+    if ( '' == $breadcrumb ) {
+        $breadcrumb = isset( $post->post_title ) ? $post->post_title : '';
+    }
+
+    $replacements['%breadcrumb%'] = $breadcrumb;
+    return $replacements;
+}
+add_filter( 'bcn_template_tags', 'my_bcn_template_tag', 3, 1 );
