47d93726ee9935a7f672ee41e26ea5eecde7376f
David Schultz <david.schultz@icecube.wisc.edu>
2022-11-15 14:45:56 -0600

    fix linting

--- includes/password-protection.php
+++ includes/password-protection.php
@@ -2,7 +2,7 @@
 
 // remove password-protected posts from all listings if you're not an admin
 function wpb_password_post_filter( $where = '' ) {
-    if ( !is_single() && !is_admin() ) {
+    if ( ! is_single() && ! is_admin() ) {
         $where .= " AND post_password = ''";
     }
     return $where;
