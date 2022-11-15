ef8fca45449b8e30eea2c66dd04e47a474d8e1db
David Schultz <david.schultz@icecube.wisc.edu>
2022-11-15 11:32:12 -0600

    fix php lint

--- blocks-src/index.php
+++ blocks-src/index.php
@@ -7,7 +7,7 @@ $dirpath = plugin_dir_path( __FILE__ );
 $scanned_directory = array_diff( scandir( $dirpath ), array( '..', '.', 'index.php' ) );
 
 foreach ( $scanned_directory as $file ) {
-    $filepath = $dirpath . trailingslashit($file) . 'index.php';
+    $filepath = $dirpath . trailingslashit( $file ) . 'index.php';
     if ( is_file( $filepath ) ) {
         require_once $filepath;
     }
