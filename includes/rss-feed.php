f523f37726c959c76b837a332e22d2af73fb2909
David Schultz <david.schultz@icecube.wisc.edu>
2022-12-07 19:44:03 -0600

    fix php linting

--- includes/rss-feed.php
+++ includes/rss-feed.php
@@ -7,12 +7,12 @@ function add_post_featured_image_as_rss_item_enclosure() {
     }
 
     $thumbnail_id = get_post_thumbnail_id( $post->ID );
-    $thumbnail_src = wp_get_attachment_image_src( $thumbnail_id , 'medium' )[0];
+    $thumbnail_src = wp_get_attachment_image_src( $thumbnail_id, 'medium' )[0];
 
-    printf( 
+    printf(
         '<enclosure url="%s" type="%s" />',
-        $thumbnail_src, 
-        get_post_mime_type( $thumbnail_id ) 
+        esc_url($thumbnail_src),
+        esc_attr(get_post_mime_type( $thumbnail_id ))
     );
 }
 add_action( 'rss2_item', 'add_post_featured_image_as_rss_item_enclosure' );
